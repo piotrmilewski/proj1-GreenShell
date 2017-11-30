ea3c73c97e9daea552228156233e7c514e1d9a21
Piotr Milewski <pmilewski@stuy.edu>
2017-11-30 15:16:54 -0500

    done with 1 and 2

--- shell.c
+++ shell.c
@@ -5,69 +5,72 @@ const char * PROMPT = "\e[1;4;32m%s@%s:%s$\e[0m ";
 
 // Returns whether an arg is empty
 int is_arg_empty(char *arg) {
-    while(*arg) {
-        if (!isspace((unsigned char)*arg)) {
-            return 0;
-        }
-        arg++;
+  while(*arg) {
+    if (!isspace((unsigned char)*arg)) {
+      return 0;
     }
-    return 1;
+    arg++;
+  }
+  return 1;
 }
 
 void make_prompt(char *buffer) {
-    uid_t uid = geteuid();
+  uid_t uid = geteuid();
 
-    struct passwd *pw = getpwuid(uid);
+  struct passwd *pw = getpwuid(uid);
 
-    char *uname = pw->pw_name;
-    char machine[128];
-    gethostname(machine, 128);
+  char *uname = pw->pw_name;
+  char machine[128];
+  gethostname(machine, 128);
  
-    char dir[256];
-    getcwd(dir, 256);
+  char dir[256];
+  getcwd(dir, 256);
     
-    char *home_dir = pw->pw_dir;
-    int home_dir_len = strlen(home_dir);
+  char *home_dir = pw->pw_dir;
+  int home_dir_len = strlen(home_dir);
     
 
-    // Find our home dir so we can shorten our directory path
-    char *home_dir_buffer = malloc(home_dir_len);
-    strncpy(home_dir_buffer, dir, home_dir_len);
-    home_dir_buffer[home_dir_len] = '\0';
+  // Find our home dir so we can shorten our directory path
+  char *home_dir_buffer = malloc(home_dir_len);
+  strncpy(home_dir_buffer, dir, home_dir_len);
+  home_dir_buffer[home_dir_len] = '\0';
 
-    // If the start of our dir is the same as our home dir,
-    // insert a ~ to shorten our home dir
-    if (!strcmp(home_dir_buffer, home_dir)) {
-        int result_len = strlen(dir) - home_dir_len;
-        char *modified_dir = malloc( 1 + result_len);
-        memcpy( &modified_dir[1], &dir[home_dir_len], result_len);
-        modified_dir[0] = '~';
-        modified_dir[1 + result_len] = '\0';
-        sprintf(buffer, PROMPT, uname, machine, modified_dir);
-    }
+  // If the start of our dir is the same as our home dir,
+  // insert a ~ to shorten our home dir
+  if (!strcmp(home_dir_buffer, home_dir)) {
+    int result_len = strlen(dir) - home_dir_len;
+    char *modified_dir = malloc( 1 + result_len);
+    memcpy( &modified_dir[1], &dir[home_dir_len], result_len);
+    modified_dir[0] = '~';
+    modified_dir[1 + result_len] = '\0';
+    sprintf(buffer, PROMPT, uname, machine, modified_dir);
+  }
+  else{
+    sprintf(buffer, PROMPT, uname, machine, dir);
+  }
 }
 
 // Makin' me re-invent arraylist ova' hea'
 char **parse_args(char *args) {
-    char *found;
-    char **arglist = malloc(sizeof(*arglist)); 
+  char *found;
+  char **arglist = malloc(sizeof(*arglist)); 
 
-    int index = 0;
-    while( (found = strsep( &args, " ")) != NULL) {
-        // Realloc based on where we are in the array
-        if (is_arg_empty(found))
-            continue;
-
-        arglist = realloc(arglist, (index + 1) * sizeof(*arglist));
-        arglist[index] = found;
-
-        index++;
-    }
+  int index = 0;
+  while( (found = strsep( &args, " ")) != NULL) {
+    // Realloc based on where we are in the array
+    if (is_arg_empty(found))
+      continue;
 
     arglist = realloc(arglist, (index + 1) * sizeof(*arglist));
-    arglist[index] = '\0';
+    arglist[index] = found;
 
-    return arglist;
+    index++;
+  }
+
+  arglist = realloc(arglist, (index + 1) * sizeof(*arglist));
+  arglist[index] = '\0';
+
+  return arglist;
 }
 
 void exec_args(char **args){
@@ -94,21 +97,21 @@ void exec_args(char **args){
 
 int main() {
 
-    char input[256];
-    char prompt[256];
+  char input[256];
+  char prompt[256];
 
-    // Programmer Senses are telling me not to do this 
-    while(1) {
-        // TODO: Replace me!
-        make_prompt(prompt);
-        printf("%s", prompt);
+  // Programmer Senses are telling me not to do this 
+  while(1) {
+    // TODO: Replace me!
+    make_prompt(prompt);
+    printf("%s", prompt);
 
-        fgets(input, sizeof(input),stdin);
-        // Remove the newline at the end of our input
-        input[strlen(input) - 1] = '\0';
+    fgets(input, sizeof(input),stdin);
+    // Remove the newline at the end of our input
+    input[strlen(input) - 1] = '\0';
 
-        exec_args(parse_args(input));
-        //printf("Result: %s", input);
-    }
-    return 0;
+    exec_args(parse_args(input));
+    //printf("Result: %s", input);
+  }
+  return 0;
 }
