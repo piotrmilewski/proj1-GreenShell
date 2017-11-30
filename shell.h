f5f2420510d189774b34d3a61c51e6d9c577ca6c
adrisj7 <adrisj7@gmail.com>
2017-11-30 10:35:39 -0500

    Add ctype import for is_arg_empty and add is_arg_empty header

--- shell.h
+++ shell.h
@@ -6,7 +6,9 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <string.h>
+#include <ctype.h>
 
+int is_arg_empty(char *arg);
 void make_prompt(char *);
 char **parse_args(char *);
 void exec_args(char **);
