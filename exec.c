3595dda7ab0fb3f0497fe170a04664bcf6c1eb13
Adris <adrisj7@gmail.com>
2017-12-03 23:50:16 -0500

    Add stdout redirection without finishing stdin redirection

--- exec.c
+++ exec.c
@@ -1,9 +1,68 @@
 #include "exec.h"
 
+void run_execvp(char **args) {
+    int pid = fork();
+    if (!pid) {
+        if (execvp(args[0], args) < 0) {
+            printf("execvp failed!\n");
+        }
+        exit(0);
+    } else {
+        wait(0);
+    }
+}
+
+void copy_args(char **src_args, char *dest_args[], int to_where) {
+    int i;
+    for(i = 0; i < to_where; i++) {
+        dest_args[i] = src_args[i];
+    }
+    // Damn null terminators
+    dest_args[to_where] = NULL;
+}
+
 void exec_args(char **args){
     //if multiple commands separated by ';'
     int i = 0;
     while (args[i]){
+
+        if (!strcmp(args[i], ">")) {
+        // If we redirect stdout 
+            // Overrite stdout
+            int file_to_write = open(args[i+1], O_CREAT | O_WRONLY, 0644);
+            int stdout_copy = dup(STDOUT_FILENO);
+            dup2(file_to_write, STDOUT_FILENO);
+
+            // Make duplicate that excludes everything after the <
+            char **copied_args = (char**)malloc( (i+1) * sizeof(*args));
+            copy_args(args, copied_args, i);
+            run_execvp(copied_args);
+
+            // Free memory and reset stdout
+            free(copied_args);
+            close(file_to_write); // should this be moved down?
+            dup2(stdout_copy, STDOUT_FILENO);
+            return;
+        } else if (!strcmp(args[i], "<")) {
+        // If we redirect stdin
+            //TODO: Figure out how stdin works actually
+            // Overrite stdin
+            printf("File: %s\n", args[i+1]);
+            int file_to_write = open(args[i+1], O_WRONLY, 0644);
+            int stdin_copy = dup(STDIN_FILENO);
+            dup2(file_to_write, STDIN_FILENO);
+
+            // Make duplicate that excludes everything after the <
+            char **copied_args = (char**)malloc( (i+1) * sizeof(*args));
+            copy_args(args, copied_args, i);
+            run_execvp(copied_args);
+
+            // Free memory and reset stdin
+            free(copied_args);
+            dup2(stdin_copy, STDIN_FILENO);
+            close(file_to_write); // should this be moved down?
+            return;
+        }
         if (strchr(args[i], ';')){
             args[i] = 0;
             exec_args(args);
@@ -24,17 +83,7 @@ void exec_args(char **args){
     }
     //execute everything else
     else{
-        int pid = fork();
-        // If we're the child
-        if (!pid) {
-            //printf("arglist: %s, %s, %s\n", arglist[0], arglist[1], arglist[2]);
-            if (execvp(args[0], args) < 0) {
-                printf("execvp failed!\n");
-            }
-            exit(0);
-        } else {
-            wait(0);
-        }
+        run_execvp(args);
     }
 }
 
